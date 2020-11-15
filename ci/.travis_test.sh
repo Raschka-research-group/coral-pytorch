4ce1b24cfc7adf5d2f36f630c36fdd0fc4dc2e09
rasbt <mail@sebastianraschka.com>
2020-11-15 17:47:08 -0600

    specify doctest folders

--- ci/.travis_test.sh
+++ ci/.travis_test.sh
@@ -7,7 +7,7 @@ python -c "import torch; print('pytorch %s' % torch.__version__)"
 
 
 if [[ "$COVERAGE" == "true" ]]; then
-     PYTHONPATH='.' pytest -sv --cov=pytorch_coral --doctest-modules
+     PYTHONPATH='.' pytest -sv --cov=pytorch_coral --doctest-modules coral_pytorch
 else
-     PYTHONPATH='.' pytest -sv --doctest-modules
+     PYTHONPATH='.' pytest -sv --doctest-modules coral_pytorch
 fi
